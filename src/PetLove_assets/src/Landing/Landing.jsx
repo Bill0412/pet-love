760b7f356ca6f62802436f63b5cdd45e9fe3b75a
NeoHugh <945214467@qq.com>
2021-12-11 16:43:19 +0800

    Landing: static page almost done

--- src/PetLove_assets/src/Landing/Landing.jsx
+++ src/PetLove_assets/src/Landing/Landing.jsx
@@ -2,17 +2,16 @@ import * as React from 'react';
 import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import Avatar from '@mui/material/Avatar';
-import img_avatar from './static/Avatar.png';
 import img_back from './static/back.png';
 import ResponsiveAppBar from "../components/app-bar";
-import "react-awesome-button/dist/styles.css";
-import "./Landing.module.css"
+import Box from "@mui/material/Box";
 import {
     AwesomeButton,
     AwesomeButtonProgress,
     AwesomeButtonSocial,
 } from 'react-awesome-button';
+import 'react-awesome-button/dist/themes/theme-rickiest.css';
+import './Landing.css';
 
 const Landing = (props) => {
     const font1Anton = "'Anton', sans-serif";
@@ -22,7 +21,7 @@ const Landing = (props) => {
         typography: {
             fontFamily: font1Anton,
             fontSize: 16,
-            color:"#123456"
+            color: "#123456"
         }
     });
     const styleCourgette = createTheme({
@@ -33,18 +32,18 @@ const Landing = (props) => {
     });
     const styleSmooch = createTheme({
         typography: {
-            fontFamily: font3Smooch,
+            fontFamily: "'Roboto', sans-serif",
             color: "orange"
         },
     });
     return (
-        <div style={{margin: "auto"}}>
+        <Box className="overall-box">
             <ResponsiveAppBar/>
             <Stack
                 direction="row"
                 justifyContent="center"
                 alignItems="center"
-                spacing={10}
+                spacing={2}
             >
                 <Stack
                     direction="column"
@@ -52,63 +51,63 @@ const Landing = (props) => {
                     alignItems="center"
                     spacing={2}
                     minHeight="90vh"
-                    maxWidth="30vw"
+                    maxWidth="40vw"
                 >
                     <ThemeProvider theme={stylesPress}>
                         <Stack direction="row"
                                justifyContent="center"
                                alignItems="center"
                                spacing={2}>
-                            <Stack maxWidth="50vw">
-                                <Typography variant="h1" component="div" gutterBottom className="main-title" >
+                            <Stack>
+                                <Typography variant="h1" component="div" gutterBottom className="main-title">
                                     Pet Love
                                 </Typography>
                             </Stack>
-                            <Stack maxWidth="50vw">
-                                <Avatar src={img_avatar} alt={"img_avatar"} sx={{width: 100, height: 100}}/>
-                            </Stack>
+                            {/*<Stack maxWidth="50vw">*/}
+                            {/*    <Avatar src={img_avatar} alt={"img_avatar"} sx={{width: 100, height: 100}}/>*/}
+                            {/*</Stack>*/}
                         </Stack>
                         <Stack>
-                            <AwesomeButton
-                                size="large"
-                                type="secondary"
-
-                            >
-                                Join Now
-                            </AwesomeButton>
-
+                            <ThemeProvider theme={styleSmooch}>
+                                <Typography variant="subtitle1" gutterBottom component="div">
+                                    In the pet love app, you can share a cute pet with your lover.<br/>
+                                    It is unique as proof of your love.<br/>
+                                    You will only have one puppy. <br/>
+                                    And its age will witness the longer your love lasts<br/>
+                                    When love breaks down, <br/>or you want to have a new dog, <br/>you can pick a new
+                                    dog in the mall.<br/>
+                                    Of course this is something we dont want to happen.<br/>
+                                    Now, start your love journey<br/>
+                                </Typography>
+                            </ThemeProvider>
                         </Stack>
+                        <Stack>
+                            <AwesomeButton type="secondary" >Join Now!!</AwesomeButton>
+                        </Stack>
+
                     </ThemeProvider>
                 </Stack>
                 <Stack
                     direction="column"
                     justifyContent="center"
                     alignItems="center"
-                    spacing={2}
+                    spacing={0}
                     minHeight="90vh"
-                    maxWidth="30vw"
+                    maxWidth="40vw"
                 >
-                    <ThemeProvider theme={styleCourgette}>
-                        <Typography variant="h5" component="div" gutterBottom>
-                            A pet to witness love <br/>keep it with your lover
-                        </Typography>
-                    </ThemeProvider>
-                    <ThemeProvider theme={styleSmooch}>
-                        <Typography variant="subtitle1" gutterBottom component="div">
-                            In the pet love app, you can share a cute pet
-                            with your lover. It is unique as proof of your love
-                            You will only have one puppy. And its age will
-                            witness the longer your love lasts<br/>
-                            When love breaks down, or you want to
-                            have a new dog, you can pick a new dog in the
-                            mall. Of course this is something we dont want
-                            to happen.<b/>
-                            Now, start your love journey
-                        </Typography>
-                    </ThemeProvider>
-                    <img src={img_back} alt="img_back"  />
+
+                    <Stack spaceing={0}>
+                        <img src={img_back} alt="img_back" />
+                    </Stack>
+                    <Stack>
+                        <ThemeProvider theme={styleCourgette}>
+                            <Typography variant="h5" component="div" gutterBottom>
+                                A pet to witness love <br/>keep it with your lover
+                            </Typography>
+                        </ThemeProvider>
+                    </Stack>
                 </Stack>
             </Stack>
-        </div>)
+        </Box>)
 }
 export default Landing;
