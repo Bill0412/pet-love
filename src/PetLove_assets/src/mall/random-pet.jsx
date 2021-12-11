9e340b4c9f4a2b7a5e08ef642a450b3eff5bde30
fenghe <jxphxufh@gmail.com>
2021-12-11 13:38:34 +0800

    update demo pet

--- src/PetLove_assets/src/mall/random-pet.jsx
+++ src/PetLove_assets/src/mall/random-pet.jsx
@@ -1,37 +1,33 @@
 import * as React from "react";
 import Grid from '@mui/material/Grid';
 import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-
-import ResponsiveAppBar from "../components/app-bar";
 import ReplayIcon from '@mui/icons-material/Replay';
 import FavoriteIcon from '@mui/icons-material/Favorite';
+import ResponsiveAppBar from "../components/app-bar";
 import GreenButton from "../components/green-button";
+import PetImage from "../../assets/images/pets/1.jpg"
 
 const RandomPetContent = () => {
   return (
     <Grid container mt={10}>
       <Grid item xs={12} md={6}>
-        <div>
-          <img src="http://placekitten.com/500/600"/>
-          &nbsp;
-          <Stack spacing={2} direction="row" justifyContent="center">
+          <Stack spacing={2} direction="column" justifyContent="center" alignItems="center">
+          <img src={PetImage} alt="Pet Image"/>
+            &nbsp;
             <GreenButton startIcon={<ReplayIcon />}>
               See Other Pets
             </GreenButton>
           </Stack>
-        </div>
       </Grid>
       <Grid item xs={12} md={6}>
         
       <Stack
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                    spacing={2}
-                    minHeight="50vh"
-                    maxWidth="30vw"
-                >
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+        minHeight="50vh"
+      >
         <Stack spacing={2} direction="column" justifyContent="center" alignItems="center" spacing={2}>
           <p>Sale: 2 ICP</p>
           <p>Age: 0 Day</p>
