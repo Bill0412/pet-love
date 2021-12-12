1a8b31024e266d3d7495144da5f706a57a31f839
fenghe <jxphxufh@gmail.com>
2021-12-12 19:21:52 +0800

    feat: pet market pagination

--- src/PetLove_assets/src/mall/pet-market.jsx
+++ src/PetLove_assets/src/mall/pet-market.jsx
@@ -1,8 +1,11 @@
 import * as React from "react";
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
+import Pagination from '@mui/material/Pagination';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+
+
 
 import ResponsiveAppBar from "../components/app-bar";
 import PetImage from "../../assets/images/pets/1.jpg"
@@ -12,36 +15,51 @@ const PetCard = (props) => {
   let item = props.item;
 
   return (
-    <Stack direction="row" spacing={2}>
-      <img src={item.img} alt="Pet Image" className="card-image"/>
-      <Stack direction="column" alignItems="center" spacing={3}>
-        <p>sale: {item.price} ICP</p>
-        <p>age: {item.age} days</p>
-        &nbsp;&nbsp;
-        <PurchaseButton label="Take me!" />
+    <Card>
+      <Stack direction="row" spacing={0} justifyContent="space-evenly" sx={{ flexWrap: 'wrap' }}>
+        <Stack direction="row" alignItems="center" justifyContent="center">
+          <img src={item.img} alt="Pet Image" className="card-image"/>
+        </Stack>
+        <Stack direction="column" alignItems="center" justifyContent="center" spacing={2}>
+            <p className="pet-detail-text">sale: {item.price} ICP</p>
+            <p className="pet-detail-text">age: {item.age} days</p>
+            <PurchaseButton label="Take me!" />
+        </Stack>
       </Stack>
-    </Stack>
+    </Card>
   );
 };
 
 const PetMarketContent = () => {
+  const [itemsPerPage, setItemsPerPage] = React.useState(6);
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const onChange = (event, page) => {
+    setCurrentPage(page);
+  }
+
+  let nPages = Math.ceil(itemData.length / itemsPerPage);
+
+  const startIndex= (currentPage - 1) * itemsPerPage;
+  const endIndex = currentPage * itemsPerPage;
+  const items = itemData.slice(startIndex, endIndex);
+
   return (
-    <div>
-      <ImageList cols={1} sx={{display: { md: 'none' }}}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.key}>
-            <PetCard item={item}/>
-          </ImageListItem>
-        ))}
-      </ImageList>
-      <ImageList spacing={4} cols={3} sx={{display: { xs: 'none', md: 'grid' }}}>
-        {itemData.map((item) => (
-          <ImageListItem key={item.key}>
-            <PetCard item={item}/>
-          </ImageListItem>
-        ))}
-      </ImageList>
-    </div>
+
+    <Stack direction="column" mt={5}>
+      <Box>
+      <Grid container spacing={2}>
+        {items.map(item =>
+          <Grid item key={item.key} xs={12} sm={6} md={4}>
+            <PetCard item={item} />
+          </Grid>
+        )}
+      </Grid>
+      </Box>
+      <Box mt={10}>
+        <Pagination count={nPages} onChange={onChange}></Pagination>
+      </Box>
+    </Stack>
   );
 };
 
@@ -56,7 +74,7 @@ const PetMarket = () => {
 
 // Should be replaced by data from backend
 var itemData = [];
-for(let i = 0; i < 16; ++i) {
+for(let i = 0; i < 40; ++i) {
   itemData.push({
     key: i,
     img: PetImage,
