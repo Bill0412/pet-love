6894df66668c52637b616b0ceb6db861bcd291c6
NeoHugh <945214467@qq.com>
2021-12-18 02:07:22 +0800

    trying to optimize gui

--- src/PetLove_assets/src/Mall/pet-market.jsx
+++ src/PetLove_assets/src/Mall/pet-market.jsx
@@ -5,70 +5,96 @@ import Pagination from '@mui/material/Pagination';
 import Grid from '@mui/material/Grid';
 import Card from '@mui/material/Card';
 import itemData from './item-data';
-
+import './pet-market.css';
 
 import ResponsiveAppBar from "../components/app-bar";
 import PurchaseButton from "./components/purchase-button";
 
 const PetCard = (props) => {
-  let item = props.item;
+    let item = props.item;
 
-  return (
-    <Card>
-      <Stack direction="row" spacing={0} justifyContent="space-evenly" sx={{ flexWrap: 'wrap' }}>
-        <Stack direction="row" alignItems="center" justifyContent="center">
-          <img src={item.img} alt="Pet Image" className="card-image"/>
-        </Stack>
-        <Stack direction="column" alignItems="center" justifyContent="center" spacing={2}>
-            <p className="pet-detail-text">sale: {item.price} ICP</p>
-            <p className="pet-detail-text">age: {item.age} days</p>
-            <PurchaseButton label="Take me!" />
-        </Stack>
-      </Stack>
-    </Card>
-  );
+    return (
+        <div className="mall-pet-card">
+            <Stack direction="row" spacing={0} justifyContent="space-evenly" sx={{flexWrap: 'wrap'}}>
+                <div className="div-pet-image">
+                    <Stack>
+                        <img src={item.img} alt="Pet Image" className="card-image"/>
+                    </Stack>
+                </div>
+                <Stack direction="column" alignItems="center" justifyContent="center" spacing={2}>
+                    <p className="pet-detail-text">sale: {item.price} ICP</p>
+                    <p className="pet-detail-text">age: {item.age} days</p>
+                    <PurchaseButton label="Take me!"/>
+                </Stack>
+            </Stack>
+        </div>
+    )
 };
 
 const PetMarketContent = () => {
-  const [itemsPerPage, setItemsPerPage] = React.useState(6);
-  const [currentPage, setCurrentPage] = React.useState(1);
+    const [itemsPerPage, setItemsPerPage] = React.useState(6);
+    const [currentPage, setCurrentPage] = React.useState(1);
 
-  const onChange = (event, page) => {
-    setCurrentPage(page);
-  }
 
-  let nPages = Math.ceil(itemData.length / itemsPerPage);
+    const onChange = (event, page) => {
+        setCurrentPage(page);
+    }
 
-  const startIndex= (currentPage - 1) * itemsPerPage;
-  const endIndex = currentPage * itemsPerPage;
-  const items = itemData.slice(startIndex, endIndex);
+    let nPages = Math.ceil(itemData.length / itemsPerPage);
 
-  return (
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = currentPage * itemsPerPage;
+    const items = itemData.slice(startIndex, endIndex);
+    let eachCardWidth = 8, ofs = 2;
+    let allColumns = eachCardWidth * 3 + ofs * 2;
 
-    <Stack direction="column" mt={5}>
-      <Box>
-      <Grid container spacing={2}>
-        {items.map(item =>
-          <Grid item key={item.key} xs={12} sm={6} md={4}>
-            <PetCard item={item} />
-          </Grid>
-        )}
-      </Grid>
-      </Box>
-      <Box mt={10}>
-        <Pagination count={nPages} onChange={onChange} />
-      </Box>
-    </Stack>
-  );
+    const RenderCard = (props) => {
+        return (
+            <Grid item xs={eachCardWidth} key={props.item.key}>
+                <PetCard item={props.item}/>
+            </Grid>
+        )
+    }
+
+    return (
+        <div className="mall-overall-height">
+            <Grid container justifyContent="center" alignItems="center" direction="column" spacing={3}>
+                <Grid container item justifyContent="center" alignItems="center" direction="row" spacing={3}
+                      className="colum-card-row" columns={allColumns}
+                >
+                    <Grid item xs={ofs}/>
+                    {[
+                        <RenderCard item={items[0]}/>, <RenderCard item={items[1]}/>, <RenderCard item={items[2]}/>
+
+                    ]}
+                    <Grid item xs={ofs}/>
+                </Grid>
+                <Grid container item justifyContent="center" alignItems="center" direction="row" spacing={3}
+                      className="colum-card-row" columns={allColumns}
+                >
+                    <Grid item xs={ofs}/>
+                    {[
+                        <RenderCard item={items[3]}/>, <RenderCard item={items[4]}/>, <RenderCard item={items[5]}/>
+                    ]}
+                    <Grid item xs={ofs}/>
+                </Grid>
+
+                <Grid item>
+                    <Pagination count={nPages} onChange={onChange}/>
+                </Grid>
+            </Grid>
+        </div>
+    )
+        ;
 };
 
 const PetMarket = () => {
-  return (
-      <div>
-          <ResponsiveAppBar/>
-          <PetMarketContent />
-      </div>
-  );
+    return (
+        <div>
+            <ResponsiveAppBar/>
+            <PetMarketContent/>
+        </div>
+    );
 };
 
 export default PetMarket;
