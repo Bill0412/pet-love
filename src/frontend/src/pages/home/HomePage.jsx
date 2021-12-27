b33fb12d5f63c69af25fedcabee812c710921b69
Xuanming Liu <liuxuanming@git.hyperchain.cn>
2021-12-28 00:17:04 +0800

    feat(home page): finish home page

--- src/frontend/src/pages/home/HomePage.jsx
+++ src/frontend/src/pages/home/HomePage.jsx
@@ -92,6 +92,42 @@ const HomePage = () => {
                         </div>
                         <br/>
                         <br/>
+                        <div className="row">
+                            <div className="col-md-3" align="center" style={{padding: "30px"}}>
+                                <h3 className="text-light"><p><i className="fas fa-wallet fa-2x"
+                                                                 style={{color: "#999"}}></i></p>Connect with your Identity
+                                </h3>
+                                <p className="text-secondary">You will be connected with your identity from Plug or II, just
+                                    click the Login Button and finish the authorization. Then you will be able to market with your
+                                    wallet.</p>
+                            </div>
+                            <div className="col-md-3" align="center" style={{padding: "30px"}}>
+                                <h3 className="text-light"><p><i className="fas fa-piggy-bank fa-2x"
+                                                                 style={{color: "#999"}}></i></p>Pick A Cute Pet
+                                </h3>
+                                <p className="text-secondary">Find a cute pet with your lover or shopping by yourself.
+                                    You can go to market to look for popular pets, or just purchase one that is new born
+                                    in this page.</p>
+                            </div>
+                            <div className="col-md-3" align="center" style={{padding: "30px"}}>
+                                <h3 className="text-light"><p><i className="fas fa-gift fa-2x"
+                                                                 style={{color: "#999"}}></i></p>Purchase It with Lover
+                                </h3>
+                                <p className="text-secondary">Then click into detail page and purchase it. You are supposed
+                                    to fill the principal of your lover, and ask for his/her agreement. Then you can own this
+                                    pet with each other. So sweet!</p>
+                            </div>
+                            <div className="col-md-3" align="center" style={{padding: "30px"}}>
+                                <h3 className="text-light"><p><i className="fas fa-venus-double fa-2x"
+                                                                 style={{color: "#999"}}></i></p>Adopt and Play with it
+                                </h3>
+                                <p className="text-secondary">Adopt the pet in your "lover zone". You can feed it or play
+                                    with it. You can have the happiness in virtual world and the pet is a witness to your
+                                    relationship.</p>
+                            </div>
+                        </div>
+                        <br/>
+                        <br/>
                         <div className="row justify-content-around explore-adj">
                             <h4 className="text-light">Newest Sold Pets </h4>
                             <ReactPlaceholder type='rect' ready={!onQuerying} showLoadingAnimation={true}
@@ -154,156 +190,6 @@ const HomePage = () => {
                                 <NFTCard size={{width: '300px', height: '350px'}}/>
                             </ReactPlaceholder>
                         </div>
-                        {/*<div className="row justify-content-around">*/}
-                        {/*    <h4 className="text-light">Fresh off the mint!</h4>*/}
-                        {/*    <ReactPlaceholder type='rect' ready={falsemint} showLoadingAnimation={true}*/}
-                        {/*                      color='#333' style={{*/}
-                        {/*        width: '300px',*/}
-                        {/*        height: '300px',*/}
-                        {/*        marginTop: '15px',*/}
-                        {/*        borderRadius: '15px'*/}
-                        {/*    }}>*/}
-                        {/*        {this.state.mimages.reverse().map((id, key) => {*/}
-                        {/*            return (*/}
-                        {/*                (key < 5) ? (*/}
-                        {/*                    (falsemint === true) ?*/}
-                        {/*                        (*/}
-                        {/*                            <div key={key} className="col-md-2 card bg-light m-3 p-2">*/}
-                        {/*                                <div className="m-2 row" align="center">*/}
-                        {/*                                    <div className="col-6">*/}
-                        {/*                                        <a href=""*/}
-                        {/*                                           onClick={(e) => this.like(e, this.state.mimageData_owner[key], this.state.mimageData_id[key])}><span*/}
-                        {/*                                            id={"count" + this.state.mimageData_id[key]}>{this.state.mimageData_likecount[key]}</span>*/}
-                        {/*                                            <i className="fa fa-heart like"*/}
-                        {/*                                               id={"like" + this.state.mimageData_id[key]}></i></a>*/}
-                        {/*                                    </div>*/}
-                        {/*                                    <div className="col-6">*/}
-                        {/*                                        <a href=""*/}
-                        {/*                                           onClick={(e) => this.ice(e, this.state.mimageData_owner[key], this.state.mimageData_id[key])}><span*/}
-                        {/*                                            id={"counti" + this.state.mimageData_id[key]}>{this.state.mimageData_icecount[key]}</span>*/}
-                        {/*                                            <i className="fas fa-gem ice"*/}
-                        {/*                                               id={"ice" + this.state.mimageData_id[key]}></i></a>*/}
-                        {/*                                    </div>*/}
-                        {/*                                </div>*/}
-                        {/*                                <Link to={{*/}
-                        {/*                                    pathname: `/nft/${this.state.mimageData_id[key]}`,*/}
-                        {/*                                    // state: {name: "vikas"}*/}
-                        {/*                                }}>*/}
-                        {/*                                    <form onSubmit={(event) => {*/}
-
-                        {/*                                    }}>*/}
-
-                        {/*                                        <div className="col-auto max-250">*/}
-
-                        {/*                                            <div className="text-secondary idbadge"*/}
-                        {/*                                                 align="center">ID*/}
-                        {/*                                                #{this.state.mimageData_id[key]}</div>*/}
-                        {/*                                            {(typeof this.state.mimageData_nftData[key] !== 'undefined') ? (*/}
-
-                        {/*                                                (this.state.mimageData_mimeType[key] === "image/jpeg" || this.state.mimageData_mimeType[key] === "image/png" || this.state.mimageData_mimeType[key] === "image/gif") ? (*/}
-                        {/*                                                    <Img alt="NFT" className="token rounded"*/}
-                        {/*                                                         src={"https://ipfs.sy.finance/ipfs/" + this.state.mimageData_nftData[key]}*/}
-                        {/*                                                         cache*/}
-                        {/*                                                         style={{background: "#000"}}/>*/}
-                        {/*                                                ) : (this.state.mimageData_mimeType[key] === "video/mp4") ? (*/}
-                        {/*                                                    <video alt="NFT"*/}
-                        {/*                                                           className="token rounded"*/}
-                        {/*                                                           autoPlay playsInline muted loop*/}
-                        {/*                                                           controls*/}
-                        {/*                                                           src={"https://ipfs.sy.finance/ipfs/" + this.state.mimageData_nftData[key]}*/}
-                        {/*                                                           type="video/mp4">*/}
-                        {/*                                                        <source*/}
-                        {/*                                                            src={"https://ipfs.sy.finance/ipfs/" + this.state.mimageData_nftData[key]}*/}
-                        {/*                                                            type="video/mp4"></source>*/}
-                        {/*                                                    </video>*/}
-                        {/*                                                ) : (this.state.mimageData_mimeType[key] === "model/gltf-binary") ? (*/}
-                        {/*                                                    <model-viewer*/}
-                        {/*                                                        src={"https://ipfs.sy.finance/ipfs/" + this.state.mimageData_nftData[key]}*/}
-                        {/*                                                        alt={this.state.mimageData_name[key]}*/}
-                        {/*                                                        ar*/}
-                        {/*                                                        ar-modes="webxr scene-viewer quick-look"*/}
-                        {/*                                                        environment-image="neutral"*/}
-                        {/*                                                        auto-rotate camera-controls style={{*/}
-                        {/*                                                        width: "100%",*/}
-                        {/*                                                        height: "250px"*/}
-                        {/*                                                    }}></model-viewer>*/}
-                        {/*                                                ) : (null)*/}
-                        {/*                                            ) : (null)*/}
-                        {/*                                            }*/}
-
-                        {/*                                        </div>*/}
-                        {/*                                        <div className="m-2"*/}
-                        {/*                                             align="center">{this.state.mimageData_name[key]}</div>*/}
-                        {/*                                        /!* <div className="m-2" align="center">{this.state.approved[key] ? ( "Price: " + this.state.imageData_price[key] )*/}
-                        {/*                        : ( "Not For Sale" )*/}
-                        {/*                        }*/}
-                        {/*                            <img alt="main" className="eth-class" src="../logo.png" />*/}
-                        {/*                        </div> *!/*/}
-
-
-                        {/*                                    </form>*/}
-                        {/*                                </Link>*/}
-                        {/*                            </div>*/}
-                        {/*                        ) : null*/}
-                        {/*                ) : null*/}
-                        {/*            )*/}
-                        {/*        })*/}
-                        {/*        }*/}
-                        {/*    </ReactPlaceholder>*/}
-                        {/*    <ReactPlaceholder type='rect' ready={falsemint} showLoadingAnimation={true}*/}
-                        {/*                      color='#333' style={{*/}
-                        {/*        width: '300px',*/}
-                        {/*        height: '300px',*/}
-                        {/*        marginTop: '15px',*/}
-                        {/*        borderRadius: '15px'*/}
-                        {/*    }}>*/}
-                        {/*        <span style={{display: "none"}}>&nbsp;</span>*/}
-                        {/*    </ReactPlaceholder>*/}
-                        {/*    <ReactPlaceholder type='rect' ready={falsemint} showLoadingAnimation={true}*/}
-                        {/*                      color='#333' style={{*/}
-                        {/*        width: '300px',*/}
-                        {/*        height: '300px',*/}
-                        {/*        marginTop: '15px',*/}
-                        {/*        borderRadius: '15px'*/}
-                        {/*    }}>*/}
-                        {/*        <span style={{display: "none"}}>&nbsp;</span>*/}
-                        {/*    </ReactPlaceholder>*/}
-                        {/*</div>*/}
-                        <br/>
-                        {/*<div className="row">*/}
-                        {/*    <div className="col-md-3" align="center" style={{padding: "30px"}}>*/}
-                        {/*        <h3 className="text-light"><p><i className="fas fa-wallet fa-2x"*/}
-                        {/*                                         style={{color: "#999"}}></i></p>Set up your wallet*/}
-                        {/*        </h3>*/}
-                        {/*        <p className="text-secondary">Once you’ve set up your wallet with Metamask, connect*/}
-                        {/*            it to Syfin by clicking the "Connect Wallet" button in the top right corner.*/}
-                        {/*            Only Metamask is supported.</p>*/}
-                        {/*    </div>*/}
-                        {/*    <div className="col-md-3" align="center" style={{padding: "30px"}}>*/}
-                        {/*        <h3 className="text-light"><p><i className="fas fa-piggy-bank fa-2x"*/}
-                        {/*                                         style={{color: "#999"}}></i></p>Add your collection*/}
-                        {/*        </h3>*/}
-                        {/*        <p className="text-secondary">Click "Mint a NFT" and set up your Syfin NFT*/}
-                        {/*            collection. You can also edit your profile to get a biography, profile avatar,*/}
-                        {/*            and name stored on chain.</p>*/}
-                        {/*    </div>*/}
-                        {/*    <div className="col-md-3" align="center" style={{padding: "30px"}}>*/}
-                        {/*        <h3 className="text-light"><p><i className="fas fa-hand-pointer fa-2x"*/}
-                        {/*                                         style={{color: "#999"}}></i></p>Mint your NFTs*/}
-                        {/*        </h3>*/}
-                        {/*        <p className="text-secondary">Upload your work (image, video, audio, or 3D model),*/}
-                        {/*            add a name, category, and description, it is cheaper than ever before with Syfin*/}
-                        {/*            on the Fantom network.</p>*/}
-                        {/*    </div>*/}
-                        {/*    <div className="col-md-3" align="center" style={{padding: "30px"}}>*/}
-                        {/*        <h3 className="text-light"><p><i className="fas fa-gavel fa-2x"*/}
-                        {/*                                         style={{color: "#999"}}></i></p>List them for sale*/}
-                        {/*        </h3>*/}
-                        {/*        <p className="text-secondary">Choose between auctions (coming soon) and fixed-price*/}
-                        {/*            listings. By selling an NFT you receive Syfin (SYF) which rewards you Spookyswap*/}
-                        {/*            (BOO)</p>*/}
-                        {/*    </div>*/}
-                        {/*</div>*/}
                     </div>
                 </div>
             </LoadingOverlay>
