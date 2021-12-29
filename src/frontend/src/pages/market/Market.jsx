50cebe1fc55f845edd3efc7dc72a67816738eb14
Xuanming Liu <liuxuanming@git.hyperchain.cn>
2021-12-29 21:35:50 +0800

    feat(market page): finish market page

--- src/frontend/src/pages/market/Market.jsx
+++ src/frontend/src/pages/market/Market.jsx
@@ -1,8 +1,11 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import ScrollToTop from "react-scroll-to-top";
 import ReactPlaceholder from "react-placeholder";
+import {Tabs, Tab, PageItem, Pagination} from "react-bootstrap"
 import {GoodCard} from "../../components/good-card/GoodCard";
 
+// Note:
+// function component is executed serially, so don't use a variable that is beyond the context.
 export default function MarketPlace() {
     // page info TODO: improve for page-pageSize
     let [totalPage, setTotalPage] = useState(1);
@@ -11,13 +14,20 @@ export default function MarketPlace() {
 
     // goods info
     let [ready, setReady] = useState(true)
-    let [totalSupply, setTotalSupply] = useState(0)
     let [goods, setGoods] = useState(Array(pageSize).fill({
         id: 123,
         name: "PetName",
         color: "Pink",
-        price: "123"
+        price: "123",
+        state: 1
     }))
+    let [totalSupply, setTotalSupply] = useState(goods.length)
+
+    // Tab choice
+    let [tabFocus, setTabFocus] = useState(0)
+    useEffect(() => {
+        // TODO obtain data again
+    }, [tabFocus])
 
     return (
         <div>
@@ -30,7 +40,8 @@ export default function MarketPlace() {
                 <div className="row justify-content-around">
                     <p align="center" className="text-secondary">This is where you can explore all pets that lived in
                         PetLove
-                        Platform. Pick one with your lover!</p>
+                        Platform. Pick one with your lover!
+                    </p>
                     <p>
                         <div align="center" style={{maxWidth: "500px", margin: "0 auto"}}><input
                             placeholder="Search for some Pets..." type="text" className="form-control my-2"
@@ -38,12 +49,40 @@ export default function MarketPlace() {
                             }} disabled={false}/></div>
                     </p>
 
-                    <ReactPlaceholder type='rect' ready={ready} showLoadingAnimation={true} color='#333'
-                                      style={{width: '300px', height: '300px', borderRadius: '15px'}}>
-                        {goods.map((good) => (
-                            <GoodCard goodInfo={good}/>
-                        ))}
-                    </ReactPlaceholder>
+                    <Tabs onSelect={(eventKey) => setTabFocus(Number(eventKey))} defaultActiveKey={tabFocus}>
+                        <Tab eventKey={0} title="All Pets" className="justify-content-around">
+                            <div className="row justify-content-around">
+                                <ReactPlaceholder type='rect' ready={ready} showLoadingAnimation={true} color='#333'
+                                                  style={{width: '300px', height: '300px', borderRadius: '15px'}}>
+                                    {goods.map((good) => (
+                                        // TODO modify state
+                                        <GoodCard goodInfo={good}/>
+                                    ))}
+                                </ReactPlaceholder>
+                            </div>
+                        </Tab>
+                        <Tab eventKey={1} title="Pets Sold in Our Platform">
+                            <div className="row justify-content-around">
+                                <ReactPlaceholder type='rect' ready={ready} showLoadingAnimation={true} color='#333'
+                                                  style={{width: '300px', height: '300px', borderRadius: '15px'}}>
+                                    {goods.map((good) => (
+                                        // TODO modify state
+                                        <GoodCard goodInfo={good}/>
+                                    ))}
+                                </ReactPlaceholder>
+                            </div>
+                        </Tab>
+                        <Tab eventKey={2} title="Pets Lived in Our Platform">
+                            <div className="row justify-content-around">
+                                <ReactPlaceholder type='rect' ready={ready} showLoadingAnimation={true} color='#333'
+                                                  style={{width: '300px', height: '300px', borderRadius: '15px'}}>
+                                    {goods.map((good) => (
+                                        <GoodCard goodInfo={good}/>
+                                    ))}
+                                </ReactPlaceholder>
+                            </div>
+                        </Tab>
+                    </Tabs>
                 </div>
             </div>
         </div>
